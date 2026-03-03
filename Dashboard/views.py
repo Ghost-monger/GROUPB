945a797d1bac681873ee6e74a27b7127686dc4d5
Ghost-monger <emmanuelmusyoka898@gmail.com>
2026-03-03 13:36:15 +0300

    Login and logout files

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate,logout
 from Dashboard.models import Student
 from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
@@ -77,3 +78,23 @@ def signup(request):
         login(request,user)
         return redirect('dashboard')
     return render(request, 'sign_up.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,
+                            username=username,
+                            password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username OR password !')
+            return redirect('login')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
